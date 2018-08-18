a6e13847e972d1d83c8879c8734276391bb0b463
Darshankolambkar <darshan.kolambkar@gmail.com>
2018-08-18 12:24:00 +0530

    Add files via upload

---
+++ service dsc.ps1
@@ -0,0 +1,17 @@
+﻿Configuration Test
+{
+   Import-DscResource -module PSDesiredStateConfiguration 
+   node @("win105.ntms.local", "localhost")
+
+   {
+        Service stopbits
+
+        {
+            Ensure = "Present"
+            name="BITS"
+            state = "running"
+            
+
+        }
+   }
+}
---
+++ servicedsc.ps1
@@ -0,0 +1,16 @@
+﻿Configuration Test
+{
+   Import-DscResource -module PSDesiredStateConfiguration 
+   node ("192.168.21.105")
+
+   {
+        Service stopbits
+
+        {
+            name='bits'
+            state = 'Stopped'
+            Ensure = 'Present'
+
+        }
+   }
+}
